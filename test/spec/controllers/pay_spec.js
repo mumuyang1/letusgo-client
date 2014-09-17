bb0a7141101540cc9df81d8a65ecd14a1fad5f26
liyanzi <843624084@qq.com>
2014-09-17 14:32:30 +0800

    finish pay test

--- test/spec/controllers/pay_spec.js
+++ test/spec/controllers/pay_spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-xdescribe('Controller: PayCtrl', function () {
+describe('Controller: PayCtrl', function () {
 
 
   beforeEach(module('letusgoApp'));
@@ -30,8 +30,8 @@ xdescribe('Controller: PayCtrl', function () {
 
       item = {barcode:'ITEM000007',category:'生活用品',name:'水杯',price:'16.00',unit:'个'};
 
-      spyOn(cartItemService, 'get').andReturn(cartProduct);
-      spyOn(cartItemService,'getTotal').andReturn(16);
+      spyOn(cartItemService, 'get').and.returnValue(cartProduct);
+      spyOn(cartItemService,'getTotal').and.returnValue(16);
   }));
 
   it('should show is ok',function(){
@@ -39,7 +39,7 @@ xdescribe('Controller: PayCtrl', function () {
       spyOn(scope,'$emit');
       createController();
       expect(scope.$emit).toHaveBeenCalledWith('to-parent-cartActive');
-      expect(cartItemService.get.calls.length).toBe(1);
+      expect(cartItemService.get).toHaveBeenCalled();
       expect(scope.cartItems.length).toBe(1);
       expect(scope.total).toBe(16);
   });
@@ -50,8 +50,8 @@ xdescribe('Controller: PayCtrl', function () {
       spyOn(scope,'$emit');
       scope.payButton(item);
       expect(scope.$emit).toHaveBeenCalledWith('to-parent-pay',item,scope.cartItems);
-      expect(cartItemService.get.calls.length).toBe(2);
-      expect(cartItemService.getTotal.calls.length).toBe(2);
+      expect(cartItemService.get.calls.count()).toBe(2);
+      expect(cartItemService.getTotal.calls.count()).toBe(2);
   });
 
 });
