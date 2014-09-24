06af921b145ee3d4d01a334332fd98338d9e2b24
liyanzi <843624084@qq.com>
2014-09-25 01:24:10 +0800

    change showing the cart list

--- app/scripts/service/itemsService.js
+++ app/scripts/service/itemsService.js
@@ -21,39 +21,8 @@ angular.module('letusgoApp').service('ItemsService',function(CartItemsService,$h
           cartSum += 1;
           CartItemsService.set('cartSum',cartSum);
 
-          this.buildCartItems(item);
+          $http.post('/api/cartItems',{'item' : item});
           return cartSum;
         };
-
-
-      this.buildCartItems = function(item){
-
-        var cartItem = {'item' : item, 'count' : 1};
-
-        $http.get('/api/cartItems')
-         .success(function (cartItems) {
-
-            if(!cartItems){
-                 cartItems= [];
-                cartItems.push(cartItem);
-
-            } else {
-              var result = judgeIsExist(cartItems,item);
-              result ? result.count++ : cartItems.push(cartItem);
-            }
-
-            $http.post('/api/cartItems',{'cartItems' : cartItems});
-        });
-      };
-
-      function judgeIsExist(cartItems,item){
-
-        for(var i = 0; i < cartItems.length; i++){
-
-          if(item.id === cartItems[i].item.id){
-            return cartItems[i];
-          }
-         }
-      };
 
 });
