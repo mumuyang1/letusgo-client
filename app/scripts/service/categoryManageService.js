885d5b49a14ee0e11a5899f5225a33816f8afa7b
liyanzi <843624084@qq.com>
2014-09-24 12:41:32 +0800

    adjust form

--- app/scripts/service/categoryManageService.js
+++ app/scripts/service/categoryManageService.js
@@ -6,7 +6,6 @@
       this.getCategories = function(callback){
           $http.get('/api/categories')
             .success(function (data) {
-//              console.log(data+'-----------');
               if(!data){
                 $http.post('/api/categories/');
               }
@@ -19,14 +18,13 @@
 
         this.getCategories(function(categories){
 
-          var data =  _.find(categories,function(category){
-
-              return category.id === id;
-
-          });
-          callback(data);
+            var data =  _.find(categories,function(category){
+                return category.id === id;
+            });
+           callback(data);
         });
-     };
+      };
+
 
       this.getCategoryByName = function(name,callback){
 
@@ -35,10 +33,9 @@
           var data =  _.find(categories,function(category){
 
             return category.name === name;
-
           });
-          callback(data);
 
+          callback(data);
         });
       };
 
@@ -46,31 +43,31 @@
       this.addCategory = function(newCategoryName,callback){
 
         this.getCategories(function(categories){
-            var id = parseInt(categories[categories.length-1].id) + 1;
-            $http.post('/api/categories/'+id, { name : newCategoryName });
-          callback();
-        });
 
+            var id = parseInt(categories[categories.length-1].id) + 1;
+
+            $http.post('/api/categories/'+id, { name : newCategoryName });
+
+            callback();
+        });
       };
 
 
       this.hasProductsInTheCategory = function(categoryId,callback){
 
           $http.get('api/items').success(function (products) {
-            console.log(categoryId+'???');
 
             callback(_.any(products,{ categoryId: categoryId}));
           });
-
       };
 
-      this.deleteCategoryButton = function(id){
 
+      this.deleteCategoryButton = function(id){
           $http.delete('/api/categories/'+id);
       };
 
-      this.changeName = function(id,newName){
 
+      this.changeName = function(id,newName){
         $http.put('/api/categories/'+id,{categoryName:newName});
       };
 });
