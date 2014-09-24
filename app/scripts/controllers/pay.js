aa333fc6f9d2e3fdf0769c79cff7c3163c9eaebc
liyanzi <843624084@qq.com>
2014-09-25 02:06:54 +0800

    can pay

--- app/scripts/controllers/pay.js
+++ app/scripts/controllers/pay.js
@@ -3,11 +3,12 @@
 angular.module('letusgoApp')
     .controller('PayCtrl', function ($scope,CartItemsService) {
 
-
       $scope.$emit('to-parent-cartActive');
-      $scope.cartItems = CartItemsService.get('cartProduct');
-      $scope.total = CartItemsService.getTotal($scope.cartItems);
 
+      CartItemsService.getCartItems(function(data){
+        $scope.cartItems = data;
+        $scope.total = CartItemsService.getTotal($scope.cartItems);
+      });
 
       $scope.payButton = function(cartItems){
           $scope.$emit('to-parent-pay',cartItems,$scope.cartItems);
