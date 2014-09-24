f247aeb1935ea96c84e531eadb935485318a44d5
liyanzi <843624084@qq.com>
2014-09-25 02:18:31 +0800

    fix show cart list bug

--- app/scripts/service/cartItemsService.js
+++ app/scripts/service/cartItemsService.js
@@ -4,12 +4,10 @@
     .service('CartItemsService', function (localStorageService,$http) {
 
       this.getCartItems= function(callback){
+        
         $http.get('/api/cartItems')
           .success(function (data) {
 
-            if(!data){
-              $http.post('/api/cartItems');
-            }
             callback(data);
           });
         };
