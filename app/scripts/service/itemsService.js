8f7b28d77525420c55493bcac126fb314bc41823
liyanzi <843624084@qq.com>
2014-10-04 19:39:24 +0800

    merge productsManageService and itemsService to one service

--- app/scripts/service/itemsService.js
+++ app/scripts/service/itemsService.js
@@ -31,5 +31,38 @@ angular.module('letusgoApp').service('ItemsService',function(CartItemsService,$h
           return cartSum;
         };
 
+        this.addProductButton = function(name,price,unit,categoryId,callback){
+
+         getAllItems(function(data){
+          var id = data[data.length - 1].id + 1;
+          $http.post('/api/items/'+id,
+            {
+              name: name,
+              price: price,
+              unit: unit,
+              categoryId: categoryId
+            });
+          callback();
+        });
+      };
+
+      this.deleteProductButton = function(id){
+        $http.delete('/api/items/'+id);
+      };
+
+
+      this.changeProduct = function(id,newName,newPrice,newUnit,newCategoryId){
+
+        $http.put('/api/items/'+id,
+          {
+            name: newName,
+            price: newPrice,
+            unit: newUnit,
+            categoryId: newCategoryId
+          }
+        );
+      };
+
+
 
 });
