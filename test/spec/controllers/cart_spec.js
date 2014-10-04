311a0b5d11714f279fc82ac5e1e825b3ca79c8b9
liyanzi <843624084@qq.com>
2014-10-04 22:01:02 +0800

    finish cart ctrl test

--- test/spec/controllers/cart_spec.js
+++ test/spec/controllers/cart_spec.js
@@ -5,79 +5,92 @@ describe('Controller: CartCtrl', function () {
 
   beforeEach(module('letusgoApp'));
 
-  var $controller,cartItemService,scope,createController,cartProduct,item;
+  var $controller,cartService,scope,createController,cartProducts,item;
 
   beforeEach(inject(function ($injector) {
     scope = $injector.get('$rootScope').$new();
     $controller = $injector.get('$controller');
 
-    cartItemService = $injector.get('CartItemsService');
+    cartService = $injector.get('CartItemsService');
 
     createController = function(){
 
       return $controller('CartCtrl', {
           $scope: scope,
-          CartItemService: cartItemService
+          CartItemsService: cartService
       });
     };
 
-    cartProduct = [
+    cartProducts = [
          {
-           items : {barcode:'ITEM000007',category:'生活用品',name:'水杯',price:'16.00',unit:'个'},
-           inputCount : 1
+           item : {id:7,barcode:'ITEM000007',category:'生活用品',name:'水杯',price:'16.00',unit:'个'},
+           count : 1
          }
       ];
 
-      item = {barcode:'ITEM000007',category:'生活用品',name:'水杯',price:'16.00',unit:'个'};
+      item = {id:7,barcode:'ITEM000007',category:'生活用品',name:'水杯',price:'16.00',unit:'个'};
 
-      spyOn(cartItemService,'set');
-      spyOn(cartItemService,'get');
-      spyOn(cartItemService,'getTotal');
       spyOn(scope,'$emit');
-
+      spyOn(cartService,'getSubtotal').and.returnValue(16);
+      spyOn(cartService,'getTotal');
+      spyOn(cartService,'getCartItems').and.callFake(function(callback){
+        callback(cartProducts);
+      });
   }));
 
-  it('should get ok',function(){
-      createController();
+  it('should highlight ok',function(){
 
+      createController();
       expect(scope.$emit).toHaveBeenCalledWith('to-parent-cartActive');
   });
 
 
-//  it('should updateTotalAndCart function ok',function(){
-//
-//      createController();
-//      expect(cartItemService.get.calls.count()).toBe(2);
-//      expect(cartItemService.getTotal).toHaveBeenCalled();
-//  });
-//
-//
+  it('can get subtotal',function(){
+
+    createController();
+    var result = scope.getSubtotal();
+    expect(result).toBe(16);
+  });
+
+  it('should updateTotalAndCart function ok',function(){
+
+      createController();
+      expect(cartService.getCartItems).toHaveBeenCalled();
+      expect(cartService.getTotal).toHaveBeenCalled();
+  });
+
+
   it('should addButton can do',function(){
 
       createController();
       scope.addButton(item);
-
       expect(scope.$emit).toHaveBeenCalledWith('to-parent-add',item);
+      expect(cartService.getCartItems).toHaveBeenCalled();
+      expect(cartService.getTotal).toHaveBeenCalled();
   });
 
   it('should reduceButton can do',function(){
-      spyOn(cartItemService,'reduceCartItem');
+      spyOn(cartService,'reduceCartItem');
 
       createController();
       scope.reduceButton(item);
 
       expect(scope.$emit).toHaveBeenCalledWith('to-parent-reduce',item);
-      expect(cartItemService.reduceCartItem).toHaveBeenCalled();
+      expect(cartService.reduceCartItem).toHaveBeenCalled();
+      expect(cartService.getCartItems).toHaveBeenCalled();
+      expect(cartService.getTotal).toHaveBeenCalled();
   });
 
   it('should deleteButton can do',function(){
-      spyOn(cartItemService,'deleteCartItem');
+      spyOn(cartService,'deleteCartItem');
 
       createController();
       scope.deleteButton(item);
 
       expect(scope.$emit).toHaveBeenCalledWith('to-parent-delete',item);
-      expect(cartItemService.deleteCartItem).toHaveBeenCalled();
+      expect(cartService.deleteCartItem).toHaveBeenCalled();
+      expect(cartService.getCartItems).toHaveBeenCalled();
+      expect(cartService.getTotal).toHaveBeenCalled();
   });
 
 });
